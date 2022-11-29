949443c0b2ca37942c2e3b4d0a78f13de36ee6c2
Ashwini H <ashh1@publicisgroupe.net>
2022-11-30 00:06:56 +0530

    Assignment files added

---
+++ src/test/java/concatenation/concat.java
@@ -0,0 +1,14 @@
+package concatenation;
+
+import org.testng.annotations.Test;
+
+public class concat {
+	@Test
+	public void Demo() {
+		String firstName="Ashwini";
+		String LastName="Harivalada";
+		System.out.println("Hello"+" "+firstName+" "+LastName);
+		
+	}
+
+}
